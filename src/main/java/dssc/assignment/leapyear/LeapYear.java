0e8a16af53025d7850c7e382acbc515f0117d40c
Matteo Ferfoglia <matteoferfoglia@gmail.com>
2020-11-20 22:48:03 +0100

    Refactor

--- src/main/java/dssc/assignment/leapyear/LeapYear.java
+++ src/main/java/dssc/assignment/leapyear/LeapYear.java
@@ -10,14 +10,9 @@ public class LeapYear {
      * Print "true" if the year is leap and "false" otherwise.
      */
     public String isLeapYear(int year){
-        if( year % 4 == 0 )
-            if( year % 100 == 0 )
-                if( year % 400 == 0 )
-                    return "true";
-                else
-                    return "false";
-            else
-                return "true";
+        if( year % 4 == 0
+                && ( year % 100 != 0 || year % 400 == 0 ) )
+            return "true";
         else
             return "false";
     }
