fd0128ea00e51118e59e1b2d0b93bbf646f256f3
Matteo Ferfoglia <matteoferfoglia@gmail.com>
2020-11-20 22:50:07 +0100

    Test refactor

--- src/test/java/dssc/assignment/leapyear/LeapYear_test.java
+++ src/test/java/dssc/assignment/leapyear/LeapYear_test.java
@@ -8,31 +8,29 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LeapYear_test {
 
+    private final LeapYear leapYear = new LeapYear();
+
     @ParameterizedTest
     @CsvSource({"2001, false", "2002, false", "2003, false"})
     void yearsDivisibleBy4(int year, String expected){
-        LeapYear leapYear = new LeapYear();
         assertEquals(expected, leapYear.isLeapYear(year));
     }
 
     @ParameterizedTest
     @CsvSource({"1996, true", "2004, true", "2008, true"})
     void yearsDivisibleBy4ButNotBy100(int year, String expected){
-        LeapYear leapYear = new LeapYear();
         assertEquals(expected,leapYear.isLeapYear(year));
     }
 
     @ParameterizedTest
     @CsvSource({"1700, false", "1800, false", "1900, false"})
     void yearsDivisibleBy4AndBy100ButNotBy400(int year, String expected){
-        LeapYear leapYear = new LeapYear();
         assertEquals(expected,leapYear.isLeapYear(year));
     }
 
     @ParameterizedTest
     @CsvSource({"1600, true", "2000, true", "2400, true"})
     void yearsDivisibleBy4AndBy100AndBy400(int year, String expected){
-        LeapYear leapYear = new LeapYear();
         assertEquals(expected,leapYear.isLeapYear(year));
     }
 
